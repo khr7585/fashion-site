a640df6182354ca0607e6ba412f28c4fbb4f93a7
Hemanth Reddy <kollahemanthreddy@gmail.com>
2026-04-26 00:07:38 +0530

    chore(ui): update homepage images

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ function goto_login() {
 function goto_create() {
   window.location.href = "../register-page/index.html";
 }
+function back(){
+  window.location.href="../index.html";
+}
 
 // //HOME-PAGE (dropdown button)
 // const btn = document.querySelector('.more-btn');
@@ -18,68 +21,68 @@ function goto_create() {
 
 
 
-// Firebase config
-const firebaseConfig = {
-  apiKey: "AIzaSyA6q899Sp9xOsuOf5A4r36AK_C_FrT1nIM",
-  authDomain: "fir-tutorial-ade86.firebaseapp.com",
-  projectId: "fir-tutorial-ade86",
-  storageBucket: "fir-tutorial-ade86.firebasestorage.app",
-  messagingSenderId: "68651479795",
-  appId: "1:68651479795:web:7eba7d8752844ce205fd86",
-};
+// // Firebase config
+// const firebaseConfig = {
+//   apiKey: "AIzaSyA6q899Sp9xOsuOf5A4r36AK_C_FrT1nIM",
+//   authDomain: "fir-tutorial-ade86.firebaseapp.com",
+//   projectId: "fir-tutorial-ade86",
+//   storageBucket: "fir-tutorial-ade86.firebasestorage.app",
+//   messagingSenderId: "68651479795",
+//   appId: "1:68651479795:web:7eba7d8752844ce205fd86",
+// };
 
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-const auth = firebase.auth();
+// // Initialize Firebase
+// firebase.initializeApp(firebaseConfig);
+// const auth = firebase.auth();
 
-// LOGIN
-document.getElementById("loginBtn").addEventListener("click", function (e) {
-  e.preventDefault();
+// // LOGIN
+// document.getElementById("loginBtn").addEventListener("click", function (e) {
+//   e.preventDefault();
 
-  const email = document.getElementById("email").value;
-  const password = document.getElementById("password").value;
+//   const email = document.getElementById("email").value;
+//   const password = document.getElementById("password").value;
 
-  auth.signInWithEmailAndPassword(email, password)
-    .then((userCredential) => {
-      alert("Login Successful ✅");
-      console.log(userCredential.user);
-    })
-    .catch((error) => {
-      alert(error.message);
-      console.error(error);
-    });
-});
+//   auth.signInWithEmailAndPassword(email, password)
+//     .then((userCredential) => {
+//       alert("Login Successful ✅");
+//       console.log(userCredential.user);
+//     })
+//     .catch((error) => {
+//       alert(error.message);
+//       console.error(error);
+//     });
+// });
 
 
-// REGISTER
-document.querySelector(".register-form").addEventListener("submit", function (e) {
-  e.preventDefault(); // stop refresh
+// // REGISTER
+// document.querySelector(".register-form").addEventListener("submit", function (e) {
+//   e.preventDefault(); // stop refresh
 
-  // const name = document.getElementById("name").value;
-  const email = document.getElementById("email").value;
-  const password = document.getElementById("password").value;
-  const confirmPassword = document.getElementById("confirmPassword").value;
+//   // const name = document.getElementById("name").value;
+//   const email = document.getElementById("email").value;
+//   const password = document.getElementById("password").value;
+//   const confirmPassword = document.getElementById("confirmPassword").value;
 
-  // Check password match
-  if (password !== confirmPassword) {
-    alert("Passwords do not match ❌");
-    return;
-  }
+//   // Check password match
+//   if (password !== confirmPassword) {
+//     alert("Passwords do not match ❌");
+//     return;
+//   }
 
-  // Create user
-  auth.createUserWithEmailAndPassword(email, password)
-    .then((userCredential) => {
-      alert("Account Created Successfully ✅");
-      console.log(userCredential.user);
+//   // Create user
+//   auth.createUserWithEmailAndPassword(email, password)
+//     .then((userCredential) => {
+//       alert("Account Created Successfully ✅");
+//       console.log(userCredential.user);
 
-      // Optional: redirect to login page
-      // window.location.href = "login.html";
-    })
-    .catch((error) => {
-      alert(error.message);
-      console.error(error);
-    });
-});
+//       // Optional: redirect to login page
+//       // window.location.href = "login.html";
+//     })
+//     .catch((error) => {
+//       alert(error.message);
+//       console.error(error);
+//     });
+// });
 
 
 
